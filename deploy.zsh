ac8361b6b8fd23c11f5b71856fe0b85a7d420d4d
Richard Lynch <lynchardrich@gmail.com>
2021-01-28 09:31:41 +0000

    data refresh Thu Jan 28 09:31:41 GMT 2021

---
+++ deploy.zsh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+conda activate base
+python update.py
+git add .
+git commit -m "data refresh $(date)"
+git push
+git branch -f gh-pages
+git checkout gh-pages
+git reset --hard origin/main
+yarn build
+cp -r dist/* .
+git add -A .
+git commit -a -m 'gh-pages update'
+git push origin gh-pages --force
+git checkout main
