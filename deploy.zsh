f4d210a8f68576b827cb8a7507eff7f985d669dd
Richard Lynch <lynchardrich@gmail.com>
2021-01-29 12:34:05 +0000

    data refresh Fri 29 Jan 2021 12:34:05 GMT

--- deploy.zsh
+++ deploy.zsh
@@ -4,12 +4,4 @@ python update.py
 git add .
 git commit -m "data refresh $(date)"
 git push
-git branch -f gh-pages
-git checkout gh-pages
-git reset --hard origin/main
-yarn build
-cp -r dist/* .
-git add -A .
-git commit -a -m 'gh-pages update'
-git push origin gh-pages --force
-git checkout main
+yarn deploy
